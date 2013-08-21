91cec50ff8ba882c676285a8e54b3c27b63b2757
Sharon Lourduraj <sharon.lourduraj@gmail.com>
2013-08-21 10:03:33 -0400

    Test

--- HeadsUp/src/org/sharonjl/headsup/MessageTable.java
+++ HeadsUp/src/org/sharonjl/headsup/MessageTable.java
@@ -2,4 +2,16 @@ package org.sharonjl.headsup;
 
 public class MessageTable {
 
+	public static final String TABLE_MESSAGE = "message";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_TIME = "messageTime";
+	public static final String COLUMN_CONTENT = "messageContent";
+
+	private static final String TABLE_CREATE = "create table " + TABLE_MESSAGE + " ("
+			+ COLUMN_ID + " integer primary key autoincrement, "
+			+ COLUMN_TIME + " integer, " 
+			+ COLUMN_CONTENT + " text); ";
+	
+	
+
 }
